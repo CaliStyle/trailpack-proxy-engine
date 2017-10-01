86aa3fabca387af5feb741a4338cd74ec0253eff
scott-wyatt <scottwyatt86@gmail.com>
2017-10-01 15:48:53 -0400

    [feat] Pluralization, Start User Stories

--- api/controllers/EventController.js
+++ api/controllers/EventController.js
@@ -16,8 +16,29 @@ module.exports = class EventController extends Controller {
     res.json({})
   }
   findAll(req, res) {
-    //
-    res.json({})
+    const Event = this.app.orm['Event']
+    const limit = Math.max(0,req.query.limit || 10)
+    const offset = Math.max(0, req.query.offset || 0)
+    const sort = req.query.sort || [['created_at', 'DESC']]
+    const where = this.app.services.ProxyEventService.jsonCritera(req.query.where)
+
+    Event.findAndCount({
+      order: sort,
+      offset: offset,
+      limit: limit,
+      where: where
+    })
+      .then(events => {
+        // Paginate
+        this.app.services.ProxyEngineService.paginate(res, events.count, limit, offset, sort)
+        return this.app.services.ProxyPermissionsService.sanitizeResult(req, events.rows)
+      })
+      .then(result => {
+        return res.json(result)
+      })
+      .catch(err => {
+        return res.serverError(err)
+      })
   }
 }
 
